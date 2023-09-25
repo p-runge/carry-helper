3bfd2582734686765fb8e342f5f98dae8100edc8
Philipp <me@p6.gg>
2023-09-25 18:06:24 +0200

    remove vertical centering of content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900">
+      <main className="flex min-h-screen flex-col items-center bg-slate-900">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <div className="flex flex-col gap-3 text-center">
             <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
